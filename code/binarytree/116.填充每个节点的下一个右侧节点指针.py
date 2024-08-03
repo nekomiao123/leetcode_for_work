d30181d46beda8fb7d6f003ef6885782f155834b
nekomiao123 <34064940+nekomiao123@users.noreply.github.com>
2024-08-04 01:32:10 +0800

    Update 116.填充每个节点的下一个右侧节点指针.py

--- code/binarytree/116.填充每个节点的下一个右侧节点指针.py
+++ code/binarytree/116.填充每个节点的下一个右侧节点指针.py
@@ -17,6 +17,23 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return None
+
+        self.traverse(root.left, root.right)
+        return root
+    
+    def traverse(self, node1: 'Optional[Node]', node2: 'Optional[Node]') -> None:
+        if not node1 or not node2:
+            return
+        
+        node1.next = node2
+
+        self.traverse(node1.left, node1.right)
+        self.traverse(node2.left, node2.right)
+        self.traverse(node1.right, node2.left)
+
+        
         
 # @lc code=end
 
