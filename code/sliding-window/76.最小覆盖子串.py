20ab9cffe767043b9da5c2612a6a2cda3a397768
nekomiao123 <34064940+nekomiao123@users.noreply.github.com>
2024-08-06 00:36:07 +0800

    Update 76.最小覆盖子串.py

--- code/sliding-window/76.最小覆盖子串.py
+++ code/sliding-window/76.最小覆盖子串.py
@@ -7,5 +7,44 @@
 # @lc code=start
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        need, window = {}, {}
+        for c in t:
+            need[c] = need.get(c, 0) + 1
+
+        left = 0
+        right = 0
+        valid = 0
+        # 记录最小覆盖子串的起始索引及长度
+        start = 0
+        length = float('inf')
+        while right < len(s):
+            # c 是将移入窗口的字符
+            c = s[right]
+            # 扩大窗口
+            right += 1
+            # 进行窗口内数据的一系列更新
+            if c in need:
+                window[c] = window.get(c, 0) + 1
+                if window[c] == need[c]:
+                    valid += 1
+
+            # 判断左侧窗口是否要收缩
+            while valid == len(need):
+                # 在这里更新最小覆盖子串
+                if right - left < length:
+                    start = left
+                    length = right - left
+                # d 是将移出窗口的字符
+                d = s[left]
+                # 缩小窗口
+                left += 1
+                # 进行窗口内数据的一系列更新
+                if d in need:
+                    if window[d] == need[d]:
+                        valid -= 1
+                    window[d] -= 1
+
+        # 返回最小覆盖子串
+        return "" if length == float('inf') else s[start: start + length]       
 # @lc code=end
 
